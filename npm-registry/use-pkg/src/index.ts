1ad83bb45562c60c0af6da4ebabec6c4362dc2e4
shinoda-yosuke-lvgs <91872986+shinoda-yosuke-lvgs@users.noreply.github.com>
2025-04-11 16:52:15 +0900

    bump hoge-pkg version to 0.0.1-2, add SubRoot export, and update use-pkg dependencies

--- npm-registry/use-pkg/src/index.ts
+++ npm-registry/use-pkg/src/index.ts
@@ -1,1 +1,7 @@
-console.log("hello world")
+import { Root } from "@shinoda-yosuke-lvgs/hoge-pkg/dist"
+import { SubRoot } from "@shinoda-yosuke-lvgs/hoge-pkg/dist/subdir"
+
+const root = new Root()
+const subRoot = new SubRoot()
+
+console.log({ root, subRoot })
